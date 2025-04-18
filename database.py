fbabf43463d572c15e0a0af8ee8a1a1216b32680
Gabriel Gama <gafityr@gmail.com>
2025-04-18 20:13:59 -0300

    correção de erros

--- database.py
+++ database.py
@@ -9,20 +9,21 @@ class Hero(SQLModel, table=True):
     age: Optional[int] = None
 
 
-def add_heros(heros_list):
+async def add_heros(heros_list: dict):
     engine = create_engine("sqlite:///data-base-hero.db")
     
     with Session(engine) as session:
         try:
-            for data_hero in heros_list:
-                hero = Hero(data_hero.get(name), data_hero.get(secret_name, data_hero.get(age)))
-                session.add(hero)
+            for hero in heros_list:
+                data_hero = Hero(name=hero.name, secret_name=hero.secret_name, age=hero.age)
+                session.add(data_hero)
             session.commit()
-        except expression as identifier:
-            print(f"Erro: {identifier}")
+
+        except Erro as error:
+            print(f"Erro: {error}")
             session.rollback()
 
-def show_all_heros() -> dict:
+async def show_all_heros() -> dict:
     engine = create_engine("sqlite:///data-base-hero.db")
 
     with Session(engine) as session:
@@ -30,7 +31,7 @@ def show_all_heros() -> dict:
             statement = select(Hero)
             results = session.exec(statement)
             heros = results.all()
-            return dict(heros)
+            return heros
 
         except expression as identifier:
-            prin(f"error na funcao show_all_heros\nErro {identifier}")
+            print(f"error na funcao show_all_heros\nErro {identifier}")
--- main.py
+++ main.py
@@ -6,25 +6,28 @@ from database import add_heros, show_all_heros
 
 class Query_hero(BaseModel):
     name: str | None = None
-    secret_name: str  
+    secret_name: str
     age: int | None = None
 
 
 class Post_heros(BaseModel):
-    name: str = | None = None
-    secret_name: str 
-    age: int = | None = None
+    name: str | None = None
+    secret_name: str
+    age: int | None = None
 
 app = FastAPI()
 
-app.post("/add heros")
+
+@app.post("/add_heros")
 async def root_add_heros(hero: Post_heros):
     add_heros(hero)
+    return {"message": "Hero added successfully"}
 
-app.get("/list heros")
+
+@app.get("/list_heros")
 async def list_heros(limit: int = 10):
     heros = show_all_heros()
-    return {[{hero.get(name): hero.value} for hero in heros]}
+    return heros
 
 #  app.get("/super hero/")
 #  async def get_parameter(parameter: Super_Heros):
